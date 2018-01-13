38091913a7ba4051834f0ed191e2b919397e3600
Sverrir Sigurðsson <sverrirs15@ru.is>
2018-01-13 15:55:19 +0000

    Downloading now works from script

--- flask/FlaskApp/flask/deildutest.py
+++ flask/FlaskApp/flask/deildutest.py
@@ -1,5 +1,8 @@
 from bs4 import BeautifulSoup
 import requests
+import urllib.request
+import os
+
 
 url_root = 'http://afghanpirate.com/'
 url_login = 'http://afghanpirate.com/takelogin.php'
@@ -26,10 +29,11 @@ with requests.Session() as s:
 
     body_element = soup.find('table', {'class': 'torrentlist'})
     movies = body_element.find_all('td', {'align': 'left'})
-    for movie in movies:
+    for movie in movies[3:]:
         if movie.a is not None and movie.a.b is not None:
             name = movie.a.b.text
             link = movie.a['href']
+            id = link.split('=')[1]
 
             print("Name: %s" % name)
             print("Link: %s" % link)
@@ -39,4 +43,14 @@ with requests.Session() as s:
             download_link = torrent_page.find('a', {'class': 'index'})['href']
             full_link = url_root + download_link
             print(full_link)
+            r = s.get(full_link)
+
+            with open('/home/sverrir/Desktop/FilmSurfer/torrents/' + id + '.torrent', 'wb') as f:
+                f.write(r.content)
+
+            print("transmission-cli -w /home/sverrir/Desktop/FilmSurfer/torrents_ready/"
+                      + " " + '/home/sverrir/Desktop/FilmSurfer/torrents/' + id + '.torrent')
+            os.system("transmission-cli -w /home/sverrir/Desktop/FilmSurfer/torrents_ready/"
+                      + " " + '/home/sverrir/Desktop/FilmSurfer/torrents/' + id + '.torrent')
+
             break
---
+++ flask/FlaskApp/flask/downloader.py
@@ -0,0 +1,4 @@
+import os
+
+statinfo = os.path.getsize('/home/sverrir/Desktop/FilmSurfer/torrents_ready/Under the Tree.mp4.part')
+print(statinfo)
