6191c5d0056c718512e0fe11779b82867961788a
Sverrir <sverrirs15@ru.is>
2018-01-13 13:37:24 +0000

    Able to get the .torrent files now

--- flask/FlaskApp/flask/deildutest.py
+++ flask/FlaskApp/flask/deildutest.py
@@ -1,12 +1,42 @@
 from bs4 import BeautifulSoup
 import requests
 
-url = raw_input('http://www.mbl.is/frettir/')
+url_root = 'http://afghanpirate.com/'
+url_login = 'http://afghanpirate.com/takelogin.php'
+url_top_movies = 'http://afghanpirate.com/browse.php?cat=6&sort=seeders&type=desc'
 
-r = requests.get(url)
+payload_login = {
+    'username': 'jobs',
+    'password': 'coolguys2083'
+}
 
-data = r.text
+def get_page(url):
+    r = s.get(url)
+    data = r.text
+    return BeautifulSoup(data, 'lxml')
 
-soup = BeautifulSoup(data)
+with requests.Session() as s:
+    r = s.post(url_login, data=payload_login)
+    r = s.get(url_top_movies)
+    data = r.text
 
-print(soup.prettify())
+    soup = BeautifulSoup(data, 'lxml')
+
+    #print(soup.prettify())
+
+    body_element = soup.find('table', {'class': 'torrentlist'})
+    movies = body_element.find_all('td', {'align': 'left'})
+    for movie in movies:
+        if movie.a is not None and movie.a.b is not None:
+            name = movie.a.b.text
+            link = movie.a['href']
+
+            print("Name: %s" % name)
+            print("Link: %s" % link)
+            print(url_root + link)
+            torrent_page = get_page(url_root + link)
+
+            download_link = torrent_page.find('a', {'class': 'index'})['href']
+            full_link = url_root + download_link
+            print(full_link)
+            break
