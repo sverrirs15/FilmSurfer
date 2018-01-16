0931832d62aeb286bc404ba48b84a4db38f296c0
Sverrir <sverrirs15@ru.is>
2018-01-16 09:53:16 +0000

    Front-End work started for real

--- film-surfer-app/FilmSurfer/App.js
+++ film-surfer-app/FilmSurfer/App.js
@@ -1,47 +1,77 @@
 import React, { Component } from 'react';
-import { ActivityIndicator, ListView, Text, View } from 'react-native';
+import { AppRegistry, StyleSheet, FlatList, Text, View, Alert, ActivityIndicator, Platform } from 'react-native';
 
 export default class Movies extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      isLoading: true
+      dataSource: [],
     }
   }
 
-  componentDidMount() {
+  componentWillMount() {
     return fetch('http://192.168.1.110:5000/get_movies')
       .then((response) => response.json())
       .then((responseJson) => {
-        let ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
         this.setState({
-          isLoading: false,
-          dataSource: ds.cloneWithRows(responseJson.movies),
-        }, function() {
-          // do something with new state
+          dataSource: responseJson.movies,
         });
-      })
-      .catch((error) => {
-        console.error(error);
+        console.log(responseJson.movies);
       });
   }
 
-  render() {
-    if (this.state.isLoading) {
-      return (
-        <View style={{flex: 1, paddingTop: 20}}>
-          <ActivityIndicator />
-        </View>
-      );
-    }
-
+  FlatListItemSeparator = () => {
     return (
-      <View style={{flex: 1, paddingTop: 20}}>
-        <ListView
-          dataSource={this.state.dataSource}
-          renderRow={(rowData) => <Text style={{flex: 1, padding: 40}}>{rowData.title}</Text>}
-        />
+      <View
+        style={{
+          height: 1,
+          width: "100%",
+          backgroundColor: "#607D8B",
+        }}
+      />
+    );
+  }
+
+  GetFlatListItem (title) {
+
+  Alert.alert(title);
+
+  }
+
+  render() {
+    return (
+      <View style={styles.MainContainer}>
+        <FlatList
+
+          data={ this.state.dataSource }
+
+          ItemSeparatorComponent = {this.FlatListItemSeparator}
+
+          renderItem={({item}) => <Text style={styles.FlatListItemStyle} onPress={this.GetFlatListItem.bind(this, item.title)} > {item.title} </Text>}
+
+          keyExtractor={(item, index) => index}
+
+         />
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+
+MainContainer :{
+
+justifyContent: 'center',
+flex:1,
+margin: 10,
+paddingTop: (Platform.OS === 'ios') ? 20 : 0,
+
+},
+
+FlatListItemStyle: {
+    padding: 10,
+    fontSize: 18,
+    height: 65,
+  },
+
+});
